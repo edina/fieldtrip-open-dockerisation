9ac7144fb64fbc22828c2ad902a5d345044a5df0
Jose Lloret <jose.lloret@ed.ac.uk>
2017-01-27 13:53:24 +0000

    baseurl for fieldtrip-viewer based on BASEURL environment variable

--- config_files/start.sh
+++ config_files/start.sh
@@ -11,8 +11,15 @@ else
 fi
 
 if [ "${BASEURL}" != "" ]; then
+    # survey-designer 
     sed -i 's|baseurl:""|baseurl:"//'${BASEURL}'"|g' ${USER_HOME}/survey-designer/dist/main.js
+    # survey-preview
     sed -i 's|"baseUrl": ""|"baseUrl": "//'${BASEURL}'"|g' ${USER_HOME}/survey-preview/dist/env.json
+    # fieldtrip-viewer
+    sed -i 's|private_base_url: \x27|private_base_url: \x27//'${BASEURL}'|g' ${USER_HOME}/fieldtrip-viewer/js/config.js
+    sed -i 's|public_base_url: \x27|public_base_url: \x27//'${BASEURL}'|g' ${USER_HOME}/fieldtrip-viewer/js/config.js
+    sed -i 's|private_image_base: "|private_image_base: "//'${BASEURL}'|g' ${USER_HOME}/fieldtrip-viewer/js/config.js
+    sed -i 's|public_image_base: \x27|public_image_base: \x27//'${BASEURL}'|g' ${USER_HOME}/fieldtrip-viewer/js/config.js
 fi
 
 /usr/sbin/apache2ctl graceful-stop
